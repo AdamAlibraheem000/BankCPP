7a2ca183b19a1a8c84b1eb8bc29accfe686cf5f6
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-05-23 20:32:24 -0400

    added Account class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<fstream>
+#include<cstdlib>
 
 using namespace std;
+
+const float MINIMUM {300.00};
+
+class Account
+{
+    private:
+    long accountNumber;
+    string firstName;
+    string lastName;
+    float balance;
+    // static var to account for next number
+    static long NextAccountNumber;
+
+    public:
+    // constructor overloading
+    Account(){}
+    Account(string fname, string lname, float balance);
+    static void setLastAccountNumber(long accountNumber);
+    static long getLastAcccountNumber();
+    friend ofstream & operator<<(ofstream &ofs,Account &acc);
+    friend ifstream & operator>>(ifstream &ifs,Account &acc);
+    friend ostream & operator<<(ostream &os,Account &acc);
+};
+
+long Account::NextAccountNumber = 0;
 
 int main(){
 
