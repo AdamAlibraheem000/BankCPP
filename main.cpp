2d8309f0986424eae13fd53732867eaa66954b72
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-05-23 21:13:32 -0400

    added Bank class

--- main.cpp
+++ main.cpp
@@ -32,6 +32,21 @@ class Account
 
 long Account::NextAccountNumber = 0;
 
+class Bank
+{
+    private:
+    map<long, Account> accounts;
+    public:
+    Bank();
+    Account OpenAccount(string fname, string lname, float balance);
+    Account BalanceEnquiry(long accountNumber);
+    Account Deposit(long accountNumber, float amount);
+    Account Withdraw(long accountNumber, float amount);
+    void CloseAccount(long accountNumber);
+    void ShowAllAccounts();
+    ~Bank();
+};
+
 int main(){
 
     cout << "What's going on?";
